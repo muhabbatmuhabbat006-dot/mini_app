4ae4b12447917ab973860fe13f6e0c441afb230f
muhabbatmuhabbat006-dot <muhabbatmuhabbat006@gmail.com>
2026-07-14 18:45:59 +0500

    Add new questions and update test logic

--- script.js
+++ script.js
@@ -2,48 +2,109 @@ const tg = window.Telegram.WebApp;
 
 tg.expand();
 
-
 let currentQuestion = 0;
 let score = 0;
 
 
 const questions = [
-    {
-        question: "Қандли диабетда асосий гормон қайси?",
-        answers: [
-            "Инсулин",
-            "Адреналин",
-            "Тироксин",
-            "Кортизол"
-        ],
-        correct: 0
-    },
 
-    {
-        question: "Қон босимини ўлчаш асбоби?",
-        answers: [
-            "Тонометр",
-            "Термометр",
-            "Фонендоскоп",
-            "ЭКГ"
-        ],
-        correct: 0
-    },
+{
+question: "Oshqozon-ichak traktining yuqumli bo‘lmagan patologiyasi tufayli kelib chiqadigan ko‘ngil aynishi va qusish sababi:",
+answers:[
+"O‘tkir qorin",
+"Ovqatdan zaharlanish",
+"Homiladorlar toksikozi",
+"Enterobioz"
+],
+correct:0
+},
 
-    {
-        question: "Тана ҳароратини ўлчаш учун?",
-        answers: [
-            "Термометр",
-            "Тонометр",
-            "Глюкометр",
-            "Спирометр"
-        ],
-        correct: 0
-    }
+
+{
+question:"Ko‘ngil aynishi va qusishning metabolik sababi:",
+answers:[
+"Migren",
+"Dori vositalaridan zaharlanish",
+"Mener kasalligi",
+"Uremiya"
+],
+correct:3
+},
+
+
+{
+question:"Ko‘ngil aynishi va qusishda davolash taktikasiga kirmaydi:",
+answers:[
+"Anamnezni sinchiklab yig‘ish",
+"Najas tahlili",
+"Homiladorlik testi",
+"Qat’iy yotoq rejimi"
+],
+correct:3
+},
+
+
+{
+question:"Yara kasalligi terapiyasi quyidagilarni o‘z ichiga oladi:",
+answers:[
+"Yotoq tartibi va maxsus parhez",
+"Aminoglikozid antibiotiklar",
+"Jarrohlik davosi",
+"Ovqatni to‘liq to‘xtatish"
+],
+correct:0
+}
+
+]; 
+
+{
+question:"Yarani jarrohlik yo‘li bilan davolashga ko‘rsatma:",
+answers:[
+"Malignizatsiya",
+"Kamqonlik",
+"Tez-tez zarda qaynashi",
+"TVI 27 dan pastligi"
+],
+correct:0
+},
+
+
+{
+question:"Yarani jarrohlik yo‘li bilan davolash asorati:",
+answers:[
+"Giperglikemiya",
+"Temir tanqisligi anemiyasi",
+"Demping sindromi",
+"Kuchli kekirish"
+],
+correct:2
+},
+
+
+{
+question:"Takroriy qusish bilan murojaat qilgan bemorda zarur:",
+answers:[
+"Serukal tavsiya qilish",
+"Oshqozonni yuvish",
+"Sababini aniqlash uchun anamnez yig‘ish",
+"Umumiy siydik tahlili"
+],
+correct:2
+},
+
+
+{
+question:"Ta’sirlangan ichak sindromida eng ko‘p uchraydigan belgi:",
+answers:[
+"Asosan ich qotishi",
+"Defekatsiya chastotasining o‘zgarishi",
+"Sutli parhezdan keyin yaxshilanish",
+"Qon ketishi"
+],
+correct:1
+}
 
 ];
-
-
 
 function startTest(){
 
@@ -63,15 +124,19 @@ function showQuestion(){
 
     document.body.innerHTML = `
 
-    <h2>${q.question}</h2>
+    <h2>${currentQuestion + 1}/8</h2>
 
-    ${q.answers.map((a,index)=>
+    <h3>${q.question}</h3>
+
+
+    ${q.answers.map((answer,index)=>
 
     `<button onclick="checkAnswer(${index})">
-    ${a}
+    ${answer}
     </button>`
 
     ).join("")}
+
 
     `;
 
@@ -85,7 +150,9 @@ function checkAnswer(answer){
 
 
     if(answer === q.correct){
+
         score++;
+
     }
 
 
@@ -96,7 +163,9 @@ function checkAnswer(answer){
 
         showQuestion();
 
-    } else {
+    }
+
+    else{
 
         finishTest();
 
@@ -108,25 +177,22 @@ function checkAnswer(answer){
 
 function finishTest(){
 
-    let result = {
+
+    tg.sendData(JSON.stringify({
 
         score: score,
 
         total: questions.length
 
-    };
+    }));
 
-
-    tg.sendData(JSON.stringify(result));
 
 
     document.body.innerHTML = `
 
     <h2>✅ Test tugadi</h2>
 
-    <p>
-    Natija: ${score}/${questions.length}
-    </p>
+    <h3>Natija: ${score}/${questions.length}</h3>
 
     `;
 
