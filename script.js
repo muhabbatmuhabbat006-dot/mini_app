e6d2ececd9654c0bdd86b972a1bf8978d2c57eae
muhabbatmuhabbat006-dot <muhabbatmuhabbat006@gmail.com>
2026-07-14 19:35:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ tg.expand();
 let currentQuestion = 0;
 let score = 0;
 
-
 const questions = [
 
 {
-question: "Oshqozon-ichak traktining yuqumli bo‘lmagan patologiyasi tufayli kelib chiqadigan ko‘ngil aynishi va qusish sababi:",
+question:"Oshqozon-ichak traktining yuqumli bo‘lmagan patologiyasi tufayli kelib chiqadigan ko‘ngil aynishi va qusish sababi:",
 answers:[
 "O‘tkir qorin",
 "Ovqatdan zaharlanish",
@@ -18,7 +17,6 @@ answers:[
 ],
 correct:0
 },
-
 
 {
 question:"Ko‘ngil aynishi va qusishning metabolik sababi:",
@@ -31,45 +29,39 @@ answers:[
 correct:3
 },
 
-
 {
 question:"Ko‘ngil aynishi va qusishda davolash taktikasiga kirmaydi:",
 answers:[
 "Anamnezni sinchiklab yig‘ish",
 "Najas tahlili",
-"Homiladorlik testi",
+"Yosh ayollarda homiladorlik testi",
 "Qat’iy yotoq rejimi"
 ],
 correct:3
 },
 
-
 {
 question:"Yara kasalligi terapiyasi quyidagilarni o‘z ichiga oladi:",
 answers:[
 "Yotoq tartibi va maxsus parhez",
-"Aminoglikozid antibiotiklar",
-"Jarrohlik davosi",
-"Ovqatni to‘liq to‘xtatish"
+"Aminoglikozidlar guruhi antibiotiklari",
+"Faqat jarrohlik davosi",
+"Oziq-ovqatni to‘liq to‘xtatish"
 ],
 correct:0
-}
-
-]; 
+},
 
 {
 question:"Yarani jarrohlik yo‘li bilan davolashga ko‘rsatma:",
 answers:[
 "Malignizatsiya",
 "Kamqonlik",
-"Tez-tez zarda qaynashi",
-"TVI 27 dan pastligi"
+"TVI 27 dan pastligi",
+"Tez-tez zarda qaynashi"
 ],
 correct:0
 },
-
-
-{
+    {
 question:"Yarani jarrohlik yo‘li bilan davolash asorati:",
 answers:[
 "Giperglikemiya",
@@ -80,120 +72,105 @@ answers:[
 correct:2
 },
 
-
 {
-question:"Takroriy qusish bilan murojaat qilgan bemorda zarur:",
+question:"Takroriy qusish shikoyati bilan murojaat qilgan bemorlarda quyidagilar zarur:",
 answers:[
-"Serukal tavsiya qilish",
+"Serukalning tavsiya qilinishi",
 "Oshqozonni yuvish",
 "Sababini aniqlash uchun anamnez yig‘ish",
-"Umumiy siydik tahlili"
+"Umumiy siydik tahlilini o‘tkazish"
 ],
 correct:2
 },
-
 
 {
 question:"Ta’sirlangan ichak sindromida eng ko‘p uchraydigan belgi:",
 answers:[
 "Asosan ich qotishi",
 "Defekatsiya chastotasining o‘zgarishi",
-"Sutli parhezdan keyin yaxshilanish",
+"Sutli parhez tayinlangandan so‘ng belgilarning yaxshilanishi",
 "Qon ketishi"
 ],
 correct:1
+},
+
+{
+question:"Ta’sirlangan ichak sindromi tashxisini qo‘yish mezoni:",
+answers:[
+"Ertalab ko‘ngil aynishi",
+"Belbog‘simon og‘riq",
+"Malina jelesi ko‘rinishidagi axlat",
+"To‘liq bo‘shatilmaganlik hissi"
+],
+correct:3
+},
+
+{
+question:"Bolalarda qabziyatni keltirib chiqaruvchi omil:",
+answers:[
+"Ratsionda fruktozaning ko‘pligi",
+"Laktoza yetishmovchiligi",
+"Ichak rivojlanishining tug‘ma patologiyasi",
+"Suvni ko‘p ichish"
+],
+correct:2
 }
 
 ];
 
-function startTest(){
-
+function startTest() {
     currentQuestion = 0;
     score = 0;
-
     showQuestion();
-
 }
 
-
-
-function showQuestion(){
-
+function showQuestion() {
     let q = questions[currentQuestion];
 
-
-    document.body.innerHTML = `
-
-    <h2>${currentQuestion + 1}/8</h2>
-
-    <h3>${q.question}</h3>
-
-
-    ${q.answers.map((answer,index)=>
-
-    `<button onclick="checkAnswer(${index})">
-    ${answer}
-    </button>`
-
-    ).join("")}
-
-
+    let html = `
+        <div class="container">
+            <h2>${currentQuestion + 1}/${questions.length}</h2>
+            <h3>${q.question}</h3>
     `;
 
+    q.answers.forEach((answer, index) => {
+        html += `
+            <button onclick="checkAnswer(${index})">
+                ${answer}
+            </button>
+        `;
+    });
+
+    html += `</div>`;
+
+    document.body.innerHTML = html;
 }
 
-
-
-function checkAnswer(answer){
-
-    let q = questions[currentQuestion];
-
-
-    if(answer === q.correct){
-
+function checkAnswer(answer) {
+    if (answer === questions[currentQuestion].correct) {
         score++;
-
     }
-
 
     currentQuestion++;
 
-
-    if(currentQuestion < questions.length){
-
+    if (currentQuestion < questions.length) {
         showQuestion();
-
-    }
-
-    else{
-
+    } else {
         finishTest();
-
     }
-
 }
 
-
-
-function finishTest(){
-
+function finishTest() {
 
     tg.sendData(JSON.stringify({
-
         score: score,
-
         total: questions.length
-
     }));
 
-
-
     document.body.innerHTML = `
-
-    <h2>✅ Test tugadi</h2>
-
-    <h3>Natija: ${score}/${questions.length}</h3>
-
+        <div class="container">
+            <h2>✅ Test tugadi</h2>
+            <h3>Natija: ${score}/${questions.length}</h3>
+        </div>
     `;
-
 }
