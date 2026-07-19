45f37169f3de2518ec83d7bae85b4c180ffd54c7
muhabbatmuhabbat006-dot <muhabbatmuhabbat006@gmail.com>
2026-07-19 05:52:01 +0500

    Add uashQuestions for medical quiz

--- tests/uash.js
+++ tests/uash.js
@@ -1,1 +1,111 @@
+const uashQuestions = [
+{
+question:"Oshqozon-ichak traktining yuqumli bo‘lmagan patologiyasi tufayli kelib chiqadigan ko‘ngil aynishi va qusish sababi:",
+answers:[
+"O‘tkir qorin",
+"Ovqatdan zaharlanish",
+"Homiladorlar toksikozi",
+"Enterobioz"
+],
+correct:0
+},
 
+{
+question:"Ko‘ngil aynishi va qusishning metabolik sababi:",
+answers:[
+"Migren",
+"Dori vositalaridan zaharlanish",
+"Mener kasalligi",
+"Uremiya"
+],
+correct:3
+},
+
+{
+question:"Ko‘ngil aynishi va qusishda davolash taktikasiga kirmaydi:",
+answers:[
+"Anamnezni sinchiklab yig‘ish",
+"Najas tahlili",
+"Yosh ayollarda homiladorlik testi",
+"Qat’iy yotoq rejimi"
+],
+correct:3
+},
+
+{
+question:"Yara kasalligi terapiyasi:",
+answers:[
+"Yotoq tartibi va maxsus parhez",
+"Aminoglikozidlar",
+"Faqat operatsiya",
+"Ovqatni to‘xtatish"
+],
+correct:0
+},
+
+{
+question:"Yarani jarrohlik davolashga ko‘rsatma:",
+answers:[
+"Malignizatsiya",
+"Kamqonlik",
+"TVI pastligi",
+"Zarda qaynashi"
+],
+correct:0
+},
+
+{
+question:"Yarani jarrohlik davolash asorati:",
+answers:[
+"Giperglikemiya",
+"Temir tanqisligi",
+"Demping sindromi",
+"Kekirish"
+],
+correct:2
+},
+
+{
+question:"Takroriy qusishda birinchi navbatda nima qilinadi?",
+answers:[
+"Serukal berish",
+"Oshqozonni yuvish",
+"Anamnez yig‘ish",
+"Operatsiya"
+],
+correct:2
+},
+
+{
+question:"Ta’sirlangan ichak sindromida asosiy belgi:",
+answers:[
+"Ich qotishi",
+"Defekatsiya chastotasining o‘zgarishi",
+"Qon ketishi",
+"Sut ichganda yaxshilanish"
+],
+correct:1
+},
+
+{
+question:"Ta’sirlangan ichak sindromi mezoni:",
+answers:[
+"Ertalab ko‘ngil aynishi",
+"Bel og‘rig‘i",
+"Malina jelesi axlat",
+"To‘liq bo‘shatilmaganlik hissi"
+],
+correct:3
+},
+
+{
+question:"Bolalarda qabziyat sababi:",
+answers:[
+"Fruktoza ko‘pligi",
+"Laktoza yetishmovchiligi",
+"Ichak tug‘ma patologiyasi",
+"Suv ko‘p ichish"
+],
+correct:2
+}
+];
